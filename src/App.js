fdc3cdc8ca5c606d39c1c8eefb799e1526d9736c
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-11-13 12:40:20 +0530

    Did Some Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+<>
+<Navbar/>
+
+<Hero/>
+
+<Footer/>
+
+</>
   );
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import bgImage from '../assests/background.png';
+import Gallery from './Gallery';
+
+function Hero() {
+
+
+  return (
+    <div className='' style={{background:"#19345F"}}>
+      
+      <Gallery/>
+    </div>
+  );
+}
+
+export default Hero;
