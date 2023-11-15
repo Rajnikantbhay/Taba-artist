310acf4525e5e7f71f51b3e5f8500122795419fd
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-11-15 14:42:57 +0530

    Chnges as On 15 Nov

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <div className='w-[50vw] bg-pink-400'>hi </div>
+      <div></div>
+      <div></div>
+      <div></div>
+   
+
+    </div>
+  )
+}
+
+export default About
