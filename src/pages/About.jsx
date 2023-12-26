5271f53db942e0635d95d0004899935386c9aa61
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-12-26 11:17:48 +0530

    Changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,45 +1,24 @@
 import React from 'react'
 
+import { ReactComponent } from '../components/RealSVG'
+
+import { RightRectangle } from '../components/RectangleRightSvg'
+
+import { LeftRectangle } from '../components/LeftRectangle'
+
+import { MainReactangle } from '../components/MainRectangle'
+
 function About() {
   return (
-    <div className='flex flex-col xl:flex-row lg:flex-row '>
 
-      <div className='bg-blue-200 '>
+    <div className='min-h-screen bg-black'>
+    <div className= 'h-[50vh] w-full '>
 
+     <MainReactangle/>
 
-        <div className='m-4 image-box  min-h-[40vh] bg-black mt-4'>
-          hi
-        </div>
-
-
-        <div className='bg-blue-200  text-xl p-2 text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore eum, officiis quibusdam veritatis dolorum amet eveniet dolor expedita deserunt itaque tenetur possimus fugiat, distinctio laudantium repellat sunt delectus voluptatem. Rerum natus, harum nemo consequuntur laboriosam culpa assumenda, tempora tempore temporibus voluptas reprehenderit, odio ducimus id! Suscipit quo quia autem deserunt. Culpa quod libero esse cum odio fuga eveniet dolore ea tenetur enim assumenda, perspiciatis adipisci sunt accusantium pariatur? Repellendus tempora corporis suscipit veritatis exercitationem rerum nisi repellat molestiae, voluptate sint magni fugit assumenda, eveniet accusantium dolor impedit odit consequatur similique aliquid! Blanditiis, omnis iusto facere eveniet delectus totam aspernatur aut ea ducimus numquam, alias dolorum possimus tempore magni distinctio commodi deserunt reiciendis eum. Excepturi tenetur hic ipsa iste esse vel ducimus explicabo ipsum. Consectetur repellendus, architecto vero atque accusamus ducimus eaque velit? Quos dolores doloribus, iusto, aperiam voluptate odit expedita culpa amet, repellat vero nobis dignissimos quasi itaque modi error harum maiores ullam ipsa? Numquam cumque nemo porro. Ipsam beatae culpa placeat libero nobis vero aspernatur cupiditate harum illum accusamus soluta, labore eos, perspiciatis vel, quidem dolor! Dicta vero repellendus ipsum ut quas, voluptates tenetur ipsa accusantium voluptatibus, at suscipit? Enim excepturi vero optio cum eius autem unde neque a soluta quod facere accusantium explicabo incidunt rem ratione corporis, repellendus minus libero quas amet, suscipit alias! Eaque neque voluptatem iusto accusantium nam iure voluptate ullam ipsa magnam aut, deserunt sapiente soluta aliquid hic doloribus vero iste voluptatibus ipsum et voluptates. Facilis repudiandae odit, enim vero tempora aspernatur a voluptas, officiis itaque ab reprehenderit nobis repellat numquam et modi molestiae, quas obcaecati. Saepe, porro quisquam molestiae esse a recusandae. Enim saepe dignissimos voluptatibus laudantium dicta provident ea officia unde totam laborum nisi praesentium rerum natus quasi modi, molestias, deserunt recusandae? Voluptatibus, placeat? Magnam earum aliquid reiciendis illum repellendus, consectetur alias labore atque explicabo suscipit sequi soluta magni deserunt neque eius distinctio provident. Quod dolores consequuntur ipsum nobis blanditiis, cum harum repellendus, recusandae quidem nemo, beatae distinctio exercitationem et illo sunt eum corporis ipsa voluptatibus? Fugiat eligendi et dicta, sunt atque fugit nostrum dolores deleniti laudantium quae magni saepe expedita aliquam ipsam ipsa dolorem id itaque, animi soluta voluptatibus facere ratione! Quidem corporis quos nulla consectetur quis odio culpa magnam aperiam possimus magni libero provident cum laboriosam velit voluptatum, numquam esse vel, impedit, veritatis enim blanditiis! Excepturi eos sequi non? Dolore ducimus, at id nam rem quisquam? Cumque quaerat cupiditate dolores iste?</div>
-      </div>
-
-
-      <div>
-
-        <div className='bg-pink-400 text-xl p-2 text-center'>
-
-          <div className='m-4 image-box  min-h-[40vh] bg-black mt-4'>
-            hi
-          </div>
-
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta id expedita, quaerat delectus ab optio nemo ducimus quos, doloribus obcaecati eum harum nesciunt incidunt aperiam? Dicta, enim molestias soluta ea rem facere corrupti blanditiis reiciendis alias harum, quis magnam quasi! Nemo vitae beatae alias officia ad consequuntur sed cupiditate debitis.</div>
-
-
-        <div className='bg-gray-200  text-xl p-2 text-center'>
-
-          <div className='m-4 image-box  min-h-[40vh] bg-black mt-4'>
-            hi
-          </div>
-
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime atque ea beatae voluptatum ut ex perferendis, fuga nisi incidunt architecto voluptatem eveniet dolore quam doloremque quas explicabo quo error. Sapiente repellat cupiditate aperiam exercitationem, commodi optio neque rerum id provident eum ea fugiat in, ab perspiciatis dolore maxime unde aspernatur consectetur nostrum aut est perferendis dolorem labore. Dignissimos, quia id quis ducimus iste blanditiis ea mollitia omnis eligendi error ratione magni beatae, accusamus doloremque facilis qui, molestiae repudiandae praesentium? Fuga itaque hic odit eius ducimus natus ipsa quo error iusto nisi qui rerum, nam odio ratione? Laborum vero autem voluptatem, dolores harum dolorem labore fuga odit pariatur itaque unde, est temporibus veritatis incidunt consequuntur soluta animi, provident corporis. Reiciendis expedita hic minus? Quaerat, cupiditate ratione odit eius sed distinctio suscipit tenetur nisi voluptas labore, itaque autem. Recusandae, consequuntur? Eos officia, rerum temporibus nemo voluptatum provident neque quae dicta similique voluptatibus repellendus alias aliquam sunt reiciendis soluta repudiandae maiores quidem et explicabo in nisi eius, modi incidunt. Veniam, repellendus, odit molestiae voluptatem labore sint blanditiis atque sit possimus voluptates cumque rerum voluptatum perferendis. Commodi, asperiores culpa libero eaque doloremque ipsam minima corrupti cupiditate. Sit illum ullam sed, earum molestias beatae qui!</div>
-
-
-      </div>
-
-
+     <div></div>
+       
+    </div>
     </div>
   )
 }
