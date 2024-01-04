cb0610708ad77820f92fba25a1899a8944414bbe
Rajnikantbhay <rsusishere@gmail.com>
2024-01-04 12:36:56 +0530

    changes

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -8,50 +8,54 @@ function Review() {
 		<div className="grid items-center gap-4 xl:grid-cols-5">
 			<div className="max-w-2xl mx-auto my-8 space-y-4 text-center xl:col-span-2 xl:text-left">
 				<h2 className="text-4xl xl:text-6xl font-redhat text-white">Reviews And Testimonials</h2>
-				<p className="dark:text-gray-400">Pri ex magna scaevola moderatius. Nullam accommodare no vix, est ei diceret alienum, et sit cetero malorum. Et sea iudico consequat, est sanctus adipisci ex.</p>
+				<p className="dark:text-gray-400">Read what have to say about us!!</p>
 			</div>
 			<div className="p-6 xl:col-span-3">
 				<div className="grid gap-4 md:grid-cols-2">
 					<div className="grid content-center gap-4">
 						<div className="p-6 rounded shadow-md dark:bg-gray-900 bg-black text-white xl:font-xl font-lg w-90vw">
-							<p className='font-lg '>An audire commodo habemus cum. Ne sed corrumpit repudiandae. Tota aliquip democritum pro in, nec democritum intellegam ne. Propriae volutpat dissentiet ea sit, nec at lorem inani tritani, an ius populo perfecto vituperatoribus. Eu cum case modus salutandi, ut eum vocent sensibus reprehendunt.</p>
+							<p className='font-lg '>I recently had the pleasure of working TABA, and I couldn't be happier with the results. Their team of artists and designers are truly talented, turning ordinary walls into breathtaking masterpieces.</p>
 							<div className="flex items-center mt-4 space-x-4">
-								<img src="https://source.unsplash.com/50x50/?portrait?1" alt="" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
+								<div className='bg-gray-500 rounded-full'>
+								<img src="https://firebasestorage.googleapis.com/v0/b/taba-5c32a.appspot.com/o/brands%20logo%20and%20extra%20svg%2Fpainterman.svg?alt=media&token=6a1cc3d1-216d-495b-8702-43d5b3cefc8c" alt="user icon" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
+								</div>
 								<div>
-									<p className="text-lg font-semibold">Leroy Jenkins</p>
-									<p className="text-sm dark:text-gray-400">CTO of Company Co.</p>
+									<p className="text-base font-inter">Adwait</p>
 								</div>
 							</div>
 						</div>
 						<div className="p-6 rounded shadow-md dark:bg-gray-900 bg-black text-white">
-							<p className='font-lg '>Sit wisi sapientem ut, pri civibus temporibus voluptatibus et, ius cu hinc fabulas. Nam meliore minimum et, regione convenire cum id. Ex pro eros mucius consectetuer, pro magna nulla nonumy ne, eam putent iudicabit consulatu cu.</p>
+							<p className='font-lg '>The seamless integration of art and design showcased their creativity and commitment to delivering a personalized and unique experience.</p>
 							<div className="flex items-center mt-4 space-x-4">
-								<img src="https://source.unsplash.com/50x50/?portrait?2" alt="" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
+								<div className='bg-gray-500 rounded-full'>
+								<img src="https://firebasestorage.googleapis.com/v0/b/taba-5c32a.appspot.com/o/brands%20logo%20and%20extra%20svg%2Fpainterman.svg?alt=media&token=6a1cc3d1-216d-495b-8702-43d5b3cefc8c" alt="user icon" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
+								</div>
 								<div>
-									<p className="text-lg font-semibold">Leroy Jenkins</p>
-									<p className="text-sm dark:text-gray-400">CTO of Company Co.</p>
+									<p className="text-base font-inter">Renukiran</p>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div className="grid content-center gap-4">
 						<div className="p-6 rounded shadow-md dark:bg-gray-900 bg-black text-white">
-							<p className='font-lg '>Putant omnium elaboraret per ut. Id dicta tritani nominavi quo, mea id justo errem elaboraret. Agam mollis scripserit ea his, ut nec postea verear persecuti. Ea noster senserit eam, ferri omittantur ei nec. Id mel solet libris efficiantur, commune explicari et eos. Case movet ad est, sed tota vocent appetere ea.</p>
+							<p className='font-lg '>From the initial consultation to the final brushstroke, the entire process was smooth and collaborative. The team was professional, punctual, and dedicated to ensuring customer satisfaction.</p>
 							<div className="flex items-center mt-4 space-x-4">
-								<img src="https://source.unsplash.com/50x50/?portrait?3" alt="" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
+								<div className='bg-gray-500 rounded-full'>
+								<img src="https://firebasestorage.googleapis.com/v0/b/taba-5c32a.appspot.com/o/brands%20logo%20and%20extra%20svg%2Fpainterman.svg?alt=media&token=6a1cc3d1-216d-495b-8702-43d5b3cefc8c" alt="user icon" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
+								</div>
 								<div>
-									<p className="text-lg font-semibold">Leroy Jenkins</p>
-									<p className="text-sm dark:text-gray-400">CTO of Company Co.</p>
+									<p className="text-base font-inter">Belita Design</p>
 								</div>
 							</div>
 						</div>
 						<div className="p-6 rounded shadow-md dark:bg-gray-900 bg-black text-white">
-							<p className='font-lg '>Te omnes virtute volutpat sed. Ei esse eros interesset vel, ei populo denique ocurreret vix, eu cum pertinax mandamus vituperatoribus. Solum nihil luptatum per ex, ei amet viderer eos. Ea illum labitur mnesarchum pro. Eius meis salutandi ei nam, alterum expetenda et nec. Expetenda intellegat at eum, per mazim sanctus honestatis ad. Ei noluisse invenire vix. Te ancillae patrioque qui, probo bonorum vivendum ex vim.</p>
+							<p className='font-lg '>What sets this company apart is their versatility. Not only do they excel in traditional wall painting, but their graphic designing skills are also top-notch. They took my ideas and preferences into account, turning them into stunning visual representations that exceeded my expectations.</p>
 							<div className="flex items-center mt-4 space-x-4">
-								<img src="https://source.unsplash.com/50x50/?portrait?4" alt="" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
+								<div className='bg-gray-500 rounded-full'>
+								<img src="https://firebasestorage.googleapis.com/v0/b/taba-5c32a.appspot.com/o/brands%20logo%20and%20extra%20svg%2Fpainterman.svg?alt=media&token=6a1cc3d1-216d-495b-8702-43d5b3cefc8c" alt="user icon" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
+								</div>
 								<div>
-									<p className="text-lg font-semibold">Leroy Jenkins</p>
-									<p className="text-sm dark:text-gray-400">CTO of Company Co.</p>
+									<p className="text-base font-inter">Martini Cafe</p>
 								</div>
 							</div>
 						</div>
